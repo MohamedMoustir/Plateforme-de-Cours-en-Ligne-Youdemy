f3e5d8544a32c441e3936ecfd5412633c84c19b0
MohamedMoustir <itsmoustir@gmail.com>
2025-01-21 23:59:58 +0100

    Implemented wishlist/index.php for managing user wishlists in the my-account section.

---
+++ Views/my-account/wishlist/index.php
@@ -0,0 +1,15 @@
+<HTML>
+<!-- Created by HTTrack Website Copier/3.49-2 [XR&CO'2014] -->
+
+<!-- Mirrored from www.radiustheme.com/demo/wordpress/themes/quiklearn/my-account/wishlist/ by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 10 Jan 2025 21:03:24 GMT -->
+<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
+<HEAD>
+<META HTTP-EQUIV="Content-Type" CONTENT="text/html;charset=UTF-8"><META HTTP-EQUIV="Refresh" CONTENT="0; URL=../../wishlist/index.html"><TITLE>Page has moved</TITLE>
+</HEAD>
+<BODY>
+<A HREF="../../wishlist/index.html"><h3>Click here...</h3></A>
+</BODY>
+<!-- Created by HTTrack Website Copier/3.49-2 [XR&CO'2014] -->
+
+<!-- Mirrored from www.radiustheme.com/demo/wordpress/themes/quiklearn/my-account/wishlist/ by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 10 Jan 2025 21:03:24 GMT -->
+</HTML>
